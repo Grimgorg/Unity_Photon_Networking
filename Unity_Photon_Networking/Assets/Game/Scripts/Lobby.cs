c584a32458214a78cc02fe8728c66dc1bff1e7ef
Grimgorg <accounts@ziel.me>
2018-03-15 20:19:50 +0100

    Player List
    Player List is implemented, but clientside...:(

--- Unity_Photon_Networking/Assets/Game/Scripts/Lobby.cs
+++ Unity_Photon_Networking/Assets/Game/Scripts/Lobby.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 
 namespace Com.GrimGames.Agility
@@ -15,8 +16,16 @@ namespace Com.GrimGames.Agility
         #region public Variables
         [Tooltip("PlayerListing Prefab")]
         public GameObject playerListingPrefab;
+        [Tooltip("PlayerListing Prefab Text")]
+        public GameObject playerListingPrefabText;
         [Tooltip("Content of PlayerList")]
         public GameObject playerListContent;
+        #endregion
+
+
+
+        #region Private Variables
+        List<GameObject> allPlayerListingPrefabs = new List<GameObject>();
         #endregion
 
 
@@ -29,6 +38,11 @@ namespace Com.GrimGames.Agility
                 GameObject startButton = GameObject.Find("Start Game Button");
                 startButton.SetActive(false);
             }
+        }
+
+        void Awake()
+        {
+                UpdatePlayerList();
         }
         #endregion
 
@@ -43,11 +57,15 @@ namespace Com.GrimGames.Agility
         public override void OnPhotonPlayerConnected(PhotonPlayer other)
         {
             Debug.Log("OnPhotonPlayerConnected() " + other.NickName);
+
+            UpdatePlayerList();
         }
 
         public override void OnPhotonPlayerDisconnected(PhotonPlayer other)
         {
             Debug.Log("OnPhotonPlayerDisconnected() " + other.NickName);
+
+            UpdatePlayerList();
         }
         #endregion
 
@@ -71,6 +89,43 @@ namespace Com.GrimGames.Agility
         public void LeaveLobby()
         {
             PhotonNetwork.LeaveRoom();
+        }
+        #endregion
+
+
+
+        #region Private Methods
+        private void UpdatePlayerList()
+        {
+
+            //GameObject[] _playerListings = GameObject.FindGameObjectsWithTag("PlayerListingPrefab");
+
+            foreach (GameObject item in allPlayerListingPrefabs)
+            {
+                Destroy(item);
+            }
+
+            //foreach (GameObject item in _playerListings)
+            //{
+            //    Destroy(item);
+            //}
+
+            foreach (PhotonPlayer currentPlayer in PhotonNetwork.playerList)
+            {
+                playerListingPrefabText.GetComponent<Text>().text = currentPlayer.NickName;
+                if (currentPlayer.IsMasterClient)
+                {
+                    playerListingPrefab.GetComponent<Image>().color = Color.yellow;
+                }
+                else
+                {
+                    playerListingPrefab.GetComponent<Image>().color = Color.white;
+                }
+
+                GameObject newPlayer = Instantiate(playerListingPrefab);
+                newPlayer.transform.SetParent(playerListContent.transform, false);
+                allPlayerListingPrefabs.Add(newPlayer);
+            }
         }
         #endregion
     }
